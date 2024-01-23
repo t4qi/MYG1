34e6fe33836e692baa8fbac63ba4f496d2eb75f5
Taqi <taqident@gmail.com>
2024-01-23 15:59:47 +0800

    Springboot huu

---
+++ my-first-rest-api/src/main/java/myg1/PatientController.java
@@ -0,0 +1,21 @@
+package myg1;
+
+import myg1.dto.Patient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+@RestController
+public class PatientController {
+
+    private final Map<Integer, Patient> map = new HashMap<>();
+@PostMapping("/patient")
+    void createPatient(@RequestBody Patient patient) {
+        map.put(patient.getPatientNo(), patient);
+    }
+
+@GetMapping("/patient/{id}")
+    Patient getPatient(@PathVariable("id") Integer patientId) {
+        return map.get(patientId);
+    }
+}
